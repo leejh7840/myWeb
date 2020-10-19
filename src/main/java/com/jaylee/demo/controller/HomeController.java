5060aee83fb682f1b50945be5da03e3b446850ad
leejh <leejh7840@gmail.com>
2020-10-19 18:19:28 -0400

    init

---
+++ src/main/java/com/jaylee/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jaylee.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "index"; //5,13:14   4,19:00
+    }
+}
